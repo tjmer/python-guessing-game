3baccda225ff7adbe6eac85e159a8e841f0cd67f
Tyler Merritt <tjmerritt5@gmail.com>
2021-09-15 22:42:36 -0600

    added feature to keep going till user stops

--- game.py
+++ game.py
@@ -1,25 +1,35 @@
 import random
-def main():
-    player_name = input("Hello, what is your name: ")
-    print("Guess a number between 1 and 100.")
 
+player_name = input("Hello, what is your name: ")
+
+
+def game_guess():
     comp_num = random.randint(1,100)
+    guess_count = 0
+    print("Guess a number between 1 and 100.")
+    player_guess = float(input("What number did comp choose?: "))
+    while player_guess != comp_num:
+        if(player_guess > comp_num):
+            guess_count += 1
+            print("You are to high!")
+            player_guess = float(input("What number did comp choose?: "))
+        if(player_guess < comp_num):
+            guess_count += 1
+            print("You are to low!")
+            player_guess = float(input("What number did comp choose?: "))
+    guess_count += 1
+    print(player_name + ", YOU GUESSED IT!  It took you", guess_count, "tries.")
 
-    def game_guess():
-        guess_count = 0
-        player_guess = float(input("What number did comp choose?: "))
-        while player_guess != comp_num:
-            if(player_guess > comp_num):
-                guess_count += 1
-                print("You are to high!")
-                player_guess = float(input("What number did comp choose?: "))
-            if(player_guess < comp_num):
-                guess_count += 1
-                print("You are to low!")
-                player_guess = float(input("What number did comp choose?: "))
-        guess_count += 1
-        print(player_name + ", YOU GUESSED IT!  It took you", guess_count, "tries.")
+def keep_going():
+    select = input("Want to play y/n: ")
+    return select
 
-
-    game_guess()
+def main():
+    continues = ''
+    while continues != "n":
+        if continues == "y":
+            game_guess()
+            continues = keep_going()
+        else:
+            continues = keep_going()
 main()
